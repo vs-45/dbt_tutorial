80174543db0c76fbd610fb8dc0f33a0479b1d4a1
Viren Shaha <viren.shaha@onixnet.com>
2025-01-09 16:12:28 +0000

    .

--- models/integration/stg/incr_test_table_1.sql
+++ models/integration/stg/incr_test_table_1.sql
@@ -12,3 +12,5 @@ FROM {{ source('my_project', 'employee') }}
 {% if is_incremental() %}
     WHERE EmployeeID > (SELECT MAX(EmployeeID) FROM {{ this }})
 {% endif %} 
+
+-- if you insert duplicate record in source then also it will not insert that in target
